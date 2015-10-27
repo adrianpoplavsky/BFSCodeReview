0ecf0b2af349bab4f5a8c81be42aa24a0da6810b
adrianpoplavsky <GitHubliopleu1>
2015-10-27 01:33:11 -0300

    Initial commit

---
+++ ConsoleApplication1/JobLogger.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace JobLogger
+{
+    public class JobLogger
+    {
+        const string ConstInsertlog = "Insert into Log(message, category) Values('{0}', {1})";
+
+        private static bool _logToFile;
+        private static bool _logToConsole;
+        private static bool _logMessage;
+        private static bool _logWarning;
+        private static bool _logError;
+        private static bool _logToDatabase;
+
+        private static JobLogger _instance;
+
+        public static JobLogger Instance
+        {
+            get { return _instance ?? (_instance = new JobLogger()); }
+        }
+
+
+        private JobLogger()
+        {
+            var logToFile = Convert.ToBoolean(ConfigurationManager.AppSettings["logToFile"]);
+            var logToConsole = Convert.ToBoolean(ConfigurationManager.AppSettings["logToConsole"]);
+            var logToDatabase = Convert.ToBoolean(ConfigurationManager.AppSettings["logToDatabase"]);
+            var logMessage = Convert.ToBoolean(ConfigurationManager.AppSettings["logMessage"]);
+            var logWarning = Convert.ToBoolean(ConfigurationManager.AppSettings["logWarning"]);
+            var logError = Convert.ToBoolean(ConfigurationManager.AppSettings["logError"]);
+
+            _logError = logError;
+            _logMessage = logMessage;
+            _logWarning = logWarning;
+            _logToDatabase = logToDatabase;
+            _logToFile = logToFile;
+            _logToConsole = logToConsole;
+        }
+
+        private enum ItemType
+        {
+            Message = 1,
+            Warning = 2,
+            Error = 3
+        }
+
+
+        public void LogMessage(string strmessage)
+        {
+            if (_logMessage)
+                LogItem(strmessage, ItemType.Message);
+        }
+
+        public void LogWarning(string strmessage)
+        {
+            if (_logWarning)
+                LogItem(strmessage, ItemType.Warning);
+        }
+
+
+        public void LogError(string strmessage)
+        {
+            if (_logError)
+                LogItem(strmessage, ItemType.Error);
+        }
+
+        public void Clean()
+        {
+            _instance = null;
+        }
+
+        static void LogItem(string strmessage, ItemType itemType)
+        {
+            if (string.IsNullOrWhiteSpace(strmessage))
+            {
+                return;
+            }
+
+            strmessage = strmessage.Trim();
+
+            if (_logToDatabase)
+            {
+                LogToDatabase(strmessage, itemType);
+            }
+
+            if (_logToFile)
+            {
+                LogToFile(strmessage);
+            }
+
+            if (_logToConsole)
+            {
+                LogToConsole(strmessage, itemType);
+            }
+        }
+
+        private static void LogToConsole(string strmessage, ItemType itemType)
+        {
+            var color = ConsoleColor.White;
+
+            switch (itemType)
+            {
+                case ItemType.Message:
+                    color = ConsoleColor.White;
+                    break;
+                case ItemType.Warning:
+                    color = ConsoleColor.Yellow;
+                    break;
+                case ItemType.Error:
+                    color = ConsoleColor.Red;
+                    break;
+            }
+
+            Console.ForegroundColor = color;
+
+            Console.WriteLine("{0}\t{1}", DateTime.Now.ToShortDateString(), strmessage);
+        }
+
+        private static void LogToFile(string strmessage)
+        {
+            var l = DateTime.Now.ToShortDateString() + "\t" + strmessage;
+            var logDirectory = ConfigurationManager.AppSettings["LogFileDirectory"];
+            var logFile = Path.Combine(logDirectory, "LogFile_" + DateTime.Now.ToString("yy-MM-dd") + ".txt");
+            if (!File.Exists(logFile))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+
+            File.AppendAllText(logFile, l);
+        }
+
+        private static void LogToDatabase(string strmessage, ItemType itemType)
+        {
+            var cmd = string.Format(ConstInsertlog, strmessage, Convert.ToInt16(itemType));
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["LogDatabaseConnectionString"].ConnectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(cmd, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
+
---
+++ JobLogger.Test/JobLoggerTests.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JobLogger.Test
+{
+    [TestClass]
+    public class JobLoggerTests
+    {
+        [TestMethod]
+        public void LogMessageToConsole()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = false.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = true.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = false.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = true.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = false.ToString();
+            ConfigurationManager.AppSettings["logError"] = false.ToString();
+
+            string output;
+            ConsoleColor consolecolor;
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    Console.Clear();
+                    JobLogger.Instance.LogMessage("a message");
+
+                    consolecolor = Console.ForegroundColor;
+
+                    output = sw.ToString();
+                }
+            }
+
+            Assert.AreEqual(ConsoleColor.White, consolecolor, "Console color does not belong to Message.");
+
+            Assert.IsTrue(output.Contains("a message"), "Console output message is incorrect.");
+        }
+
+        [TestMethod]
+        public void LogWarningToConsole()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = false.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = true.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = false.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = false.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = true.ToString();
+            ConfigurationManager.AppSettings["logError"] = false.ToString();
+
+            string output;
+            ConsoleColor consolecolor;
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    Console.Clear();
+                    JobLogger.Instance.LogWarning("a message");
+
+                    consolecolor = Console.ForegroundColor;
+
+                    output = sw.ToString();
+                }
+            }
+
+            Assert.AreEqual(ConsoleColor.Yellow, consolecolor, "Console color does not belong to Warning.");
+
+            Assert.IsTrue(output.Contains("a message"), "Console output message is incorrect.");
+        }
+
+        [TestMethod]
+        public void LogErrorToConsole()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = false.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = true.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = false.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = false.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = false.ToString();
+            ConfigurationManager.AppSettings["logError"] = true.ToString();
+
+            string output;
+            ConsoleColor consolecolor;
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    Console.Clear();
+
+                    JobLogger.Instance.LogError("a message");
+                    consolecolor = Console.ForegroundColor;
+                    output = sw.ToString();
+
+                }
+
+            }
+
+            Assert.AreEqual(ConsoleColor.Red, consolecolor, "Console color does not belong to Error.");
+
+            Assert.IsTrue(output.Contains("a message"), "Console output message is incorrect.");
+        }
+
+        [TestMethod]
+        public void DontLogToConsole()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = false.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = false.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = false.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = true.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = true.ToString();
+            ConfigurationManager.AppSettings["logError"] = true.ToString();
+
+            string output;
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    Console.Clear();
+
+                    JobLogger.Instance.LogMessage("a message");
+                    JobLogger.Instance.LogWarning("a message");
+                    JobLogger.Instance.LogError("a message");
+                    output = sw.ToString();
+                }
+            }
+
+            Assert.IsTrue(output == string.Empty, "Console output message is incorrect.");
+        }
+
+        [TestMethod]
+        public void LogMessageToFile()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = true.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = false.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = false.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = true.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = false.ToString();
+            ConfigurationManager.AppSettings["logError"] = true.ToString();
+
+
+            using (new StringWriter())
+            {
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    JobLogger.Instance.LogMessage("a message");
+                }
+            }
+
+            Assert.IsTrue(true, "file output message is incorrect.");
+        }
+        [TestMethod]
+        public void LogWarningToFile()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = true.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = false.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = false.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = false.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = true.ToString();
+            ConfigurationManager.AppSettings["logError"] = false.ToString();
+
+
+            using (new StringWriter())
+            {
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    JobLogger.Instance.LogWarning("a message");
+                }
+            }
+
+            Assert.IsTrue(true, "file output message is incorrect.");
+        }
+        [TestMethod]
+        public void LogErrorToFile()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = true.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = false.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = false.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = false.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = false.ToString();
+            ConfigurationManager.AppSettings["logError"] = true.ToString();
+
+
+            using (new StringWriter())
+            {
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    JobLogger.Instance.LogError("a message");
+                }
+            }
+
+            Assert.IsTrue(true, "file output message is incorrect.");
+        }
+
+        [TestMethod]
+        public void DontLogToFile()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = false.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = false.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = false.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = true.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = true.ToString();
+            ConfigurationManager.AppSettings["logError"] = true.ToString();
+
+            string output;
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    Console.Clear();
+
+                    JobLogger.Instance.LogMessage("a message");
+                    JobLogger.Instance.LogWarning("a message");
+                    JobLogger.Instance.LogError("a message");
+                    output = sw.ToString();
+                }
+            }
+
+            Assert.IsTrue(output == string.Empty, "Console output message is incorrect.");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [TestMethod]
+        public void LogMessageToDb()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = false.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = false.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = true.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = true.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = false.ToString();
+            ConfigurationManager.AppSettings["logError"] = true.ToString();
+
+
+            using (new StringWriter())
+            {
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    JobLogger.Instance.LogError("a message");
+                }
+            }
+
+            Assert.IsTrue(true, "file output message is incorrect.");
+        }
+        [TestMethod]
+        public void LogWarningToDb()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = false.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = false.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = true.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = false.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = true.ToString();
+            ConfigurationManager.AppSettings["logError"] = false.ToString();
+
+
+            using (new StringWriter())
+            {
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    JobLogger.Instance.LogWarning("a message");
+                }
+            }
+
+            Assert.IsTrue(true, "file output message is incorrect.");
+        }
+        [TestMethod]
+        public void LogErrorToDb()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = false.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = false.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = true.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = false.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = false.ToString();
+            ConfigurationManager.AppSettings["logError"] = true.ToString();
+
+
+            using (new StringWriter())
+            {
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    JobLogger.Instance.LogError("a message");
+                }
+            }
+
+            Assert.IsTrue(true, "file output message is incorrect.");
+        }
+
+        [TestMethod]
+        public void DontLogToDb()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = false.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = false.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = false.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = true.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = true.ToString();
+            ConfigurationManager.AppSettings["logError"] = true.ToString();
+
+            string output;
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    Console.Clear();
+
+                    JobLogger.Instance.LogMessage("a message");
+                    JobLogger.Instance.LogWarning("a message");
+                    JobLogger.Instance.LogError("a message");
+                    output = sw.ToString();
+                }
+            }
+
+            Assert.IsTrue(output == string.Empty, "Console output message is incorrect.");
+        }
+
+        [TestMethod]
+        public void BlankMessageDoesntLog()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = false.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = false.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = false.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = true.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = true.ToString();
+            ConfigurationManager.AppSettings["logError"] = true.ToString();
+
+            string output;
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                lock (new object())
+                {
+                    JobLogger.Instance.Clean();
+                    Console.Clear();
+
+                    JobLogger.Instance.LogMessage("");
+                    JobLogger.Instance.LogWarning("");
+                    JobLogger.Instance.LogError("");
+                    output = sw.ToString();
+                }
+            }
+
+            Assert.IsTrue(output == string.Empty, "Console output message is incorrect.");
+        }
+
+
+        [TestMethod]
+        public void LogDirectoryGetsCreated()
+        {
+            ConfigurationManager.AppSettings["logToFile"] = true.ToString();
+            ConfigurationManager.AppSettings["logToConsole"] = false.ToString();
+            ConfigurationManager.AppSettings["logToDatabase"] = false.ToString();
+            ConfigurationManager.AppSettings["logMessage"] = true.ToString();
+            ConfigurationManager.AppSettings["logWarning"] = true.ToString();
+            ConfigurationManager.AppSettings["logError"] = true.ToString();
+
+            var logDirectory = ConfigurationManager.AppSettings["LogFileDirectory"];
+
+            lock (new object())
+            {
+                JobLogger.Instance.Clean();
+                Console.Clear();
+
+                if (Directory.Exists(logDirectory))
+                    Directory.Delete(logDirectory, true);
+
+                JobLogger.Instance.LogMessage("a message");
+                JobLogger.Instance.LogWarning("a message");
+                JobLogger.Instance.LogError("a message");
+
+            Assert.IsTrue(Directory.Exists(logDirectory), "Directory does not exist.");
+            }
+        }
+    }
+}
